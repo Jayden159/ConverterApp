5230f74c8531e529d97875f58020adc9e7c75859
Jayden Spencer <jayden159@gmail.com>
2015-04-15 13:12:58 +0800

    converted each conversion to hashMap
    each standard method is now just a singular hashmap key and value

--- src/converterApp/ConverterFormulas.java
+++ src/converterApp/ConverterFormulas.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package converterApp;
 
 import java.util.HashMap;
@@ -13,84 +8,25 @@ import java.util.HashMap;
  */
 public class ConverterFormulas {
     
-    private HashMap ISToUS = new HashMap();
-    private double input;
-    
+    private HashMap ISToUS = new HashMap();    
     
     public ConverterFormulas(double input){
-        ISToUS.put("cmToinch", input*0.39370);
-        ISToUS.put("cmTofeet", input*0.032808);
-        ISToUS.put("cmToyard", input*0.010936);
+        ISToUS.put("cmToinch", input * 0.39370);
+        ISToUS.put("cmTofeet", input * 0.032808);
+        ISToUS.put("cmToyard", input * 0.010936);
+        ISToUS.put("cmTomile", input * 0.00000621);
+        ISToUS.put("meterToinch", input * 39.370);
+        ISToUS.put("meterTofeet", input * 3.2808);
+        ISToUS.put("meterToyard", input * 1.0936);
+        ISToUS.put("meterTomile", input * 0.00062137);
+        ISToUS.put("kiometerToinch", input * 39370);
+        ISToUS.put("kiometerTofeet", input * 3280.8);
+        ISToUS.put("kiometerToyard", input * 1093.6);
+        ISToUS.put("kiometerTomile", input * 0.62137);
+        ISToUS.put("celciusTohahrenheit", (input * 9/5) + 32);
     }
     
     public double getValue(String sourceUnit, String targetUnit){
        return (double) ISToUS.get(sourceUnit+"To"+targetUnit);
     }
-
-////cm to inch
-//    public static double cmToInch(double input, int round) {
-//        double result = input * 0.39370;
-//        //result = round;
-//        return result;
-//    }
-//    //cm to feet
-//    public static double cmToFeet(double input, int round) {
-//        double result = input * 0.032808;
-//        return result;
-//    }
-//    //cm to yards
-//    public static double cmToYard(double input, int round) {
-//        double result = input * 0.010936;
-//        return result;
-//    }
-//    //cm to miles
-//    public static double cmToMile(double input, int round) {
-//        double result = input * 0.00000621;
-//        return result;
-//    }
-//    //meters to inches
-//    public static double meterToInch(double input, int round) {
-//        double result = input * 39.370;
-//        return result;
-//    }
-//    //meters to feet
-//    public static double meterToFeet(double input, int round) {
-//        double result = input * 3.2808;
-//        return result;
-//    }
-//    //meters to yards
-//    public static double meterToYard(double input, int round) {
-//        double result = input * 1.0936;
-//        return result;
-//    }
-//    //meters to miles
-//    public static double meterToMile(double input, int round) {
-//        double result = input * 0.00062137;
-//        return result;
-//    }
-//    //kilometers to inches
-//    public static double kilometerToInch(double input, int round) {
-//        double result = input * 39370;
-//        return result;
-//    }
-//    //kilometers to feet
-//    public static double kilometerToFeet(double input, int round) {
-//        double result = input * 3280.8;
-//        return result;
-//    }
-//    //kilometers to yards
-//    public static double kilometerToYard(double input, int round) {
-//        double result = input * 1093.6;
-//        return result;
-//    }
-//    //kilometers to miles
-//    public static double kilometerToMile(double input, int round) {
-//        double result = input * 0.62137;
-//        return result;
-//    }
-//    //cesius to fahrenheit
-//    public static double celsiusToFahrenheit(double input, int round) {
-//        double result = (input * 9/5) + 32;
-//        return result;
-//    }
 }
